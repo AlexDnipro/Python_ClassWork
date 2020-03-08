d56bb580ad3078bdf5cd443b3a1f698b64bc992b
AlexDnipro <olexiyusov@gmail.com>
2020-03-08 20:17:54 +0000

    lists/iteration

---
+++ cw_11_02_20.py
@@ -0,0 +1,56 @@
+# Part 1
+# num = int(input("Please enter the number of integers:"))
+# spisok = []
+# for i in range(0, num):
+#     spisok.append(int(input("Please enter the number: {}".format(i))))
+
+# print("Minimum is {}, Maximum is {}". format(min(spisok), max(spisok)))
+
+# Part 2
+# massiv = []
+# factors3 = []
+# evens = []
+# notBy2nor3 = []
+# for i in range(1,11):
+#     massiv.append(i)
+
+# evens = [ k for k in massiv if k % 2 == 0]
+# factors3 = [ k  for k in massiv if k % 2 != 0 and k %3 == 0]
+# notBy2nor3 = [ k  for k in massiv if k % 2 != 0 and k % 3 != 0 ]
+# print(evens)
+# print(factors3)
+# print(notBy2nor3)
+
+# Part 3
+# factorial = 1
+# number = int(input("Please enter the number:"))
+# for i in range(2,number + 1):
+#     factorial = factorial * i
+
+# print(factorial)
+
+# Part 4
+
+# while ( input("Please enter the password:") != "First"):
+#     continue
+
+# Part 5
+
+# while ( int(input("Please enter the number:")) > 0):
+#     continue
+
+# Part 6
+#iterate = int(input("Please enter the number of integers:"))
+
+# for j in range(iterate):
+#     if int(input("Please enter the number:")) > 0 :
+#         continue
+#     else: break
+
+# Part 7
+
+for i in range(10, 31):
+    if ():
+        print("2 * {}".format(i//2) )
+    else: 
+       print("{} is prime number".format(i))
